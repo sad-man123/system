b22237a9ba00ad8cf0ab4b39aadb0dd058040b2d
cute femboy <sadman0897@gmail.com>
2024-09-14 13:18:39 +0300

    library manager is completed

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import subprocess
+import os, json
 
-try:
-    import gay
-except Exception as ex:
-    print(ex)
+new_lib = True
+while True:
+    try:
+        import gay, porno
+        break
+    except Exception as ex:
+        if new_lib:
+            with open("lib.json") as f:
+                lib = json.loads(f.read())
+                new_lib = False
+        ex1 = str(ex)
+        ex1 = ex1.replace("No module named ", "")
+        ex1 = ex1.replace("'", "")
+        if ex1 in lib:
+            ex1 = lib[ex1]
+        print(ex1)
+        try:
+            os.system(f"pip install {ex1}")
+        except:
+            print("not install lib")
